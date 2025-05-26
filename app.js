f2fa3a152a1de642db0ae1ce9ecb1a384e4d946f
DivineFF <maciej.jaki28@gmail.com>
2025-05-26 16:10:28 +0200

    Home changes

--- app.js
+++ app.js
@@ -5,3 +5,36 @@ menu.addEventListener('click', function(){
     menu.classList.toggle('is-active')
     menulinks.classList.toggle('active');
 });
+ 
+  /*window.addEventListener('load', () => {
+  setTimeout(() => {
+    const homeSection = document.getElementById('home');
+    if (homeSection) {
+      homeSection.scrollIntoView({ behavior: 'smooth' });
+    }
+  }, 3000); // opóźnienie 3 sekundy
+});*/
+
+document.addEventListener('DOMContentLoaded', function () {
+  const scrollBtn = document.getElementById('scrollBtn');
+  if (scrollBtn) {
+    scrollBtn.addEventListener('click', function (e) {
+      e.preventDefault();
+      const homeSection = document.getElementById('home');
+      if (homeSection) {
+        homeSection.scrollIntoView({ behavior: 'smooth' });
+      }
+    });
+  }
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+  const introSection = document.getElementById('intro');
+  const homeSection = document.getElementById('home');
+
+  if (introSection && homeSection) {
+    introSection.addEventListener('click', function () {
+      homeSection.scrollIntoView({ behavior: 'smooth' });
+    });
+  }
+});
